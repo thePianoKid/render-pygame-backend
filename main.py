7a2b998bb08cc88dd66cdbd1b5600b01edb3625e
Gabriel Braden <89939656+thePianoKid@users.noreply.github.com>
2024-03-02 04:17:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from typing import Optional
+from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# r: the mobile phone user will be assigned to move right
+# l: the mobile phone user will be assigned to move left
+# u: the mobile phone user will be assigned to move up
+# s: the mobile phone user will be assigned to shoot
+actions = ["s", "u", "l", "r"]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/actions")
+def read_root():
+    if len(actions) > 0:
+        action = actions.pop()
+    else:
+        action = None
+        
+    return {"action": action}
